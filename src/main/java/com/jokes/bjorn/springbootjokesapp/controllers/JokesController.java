298de30cdf8633a0fe38b14951944547ec78b32b
beorn <beorn_d_latch@yahoo.com>
2018-10-10 17:58:29 +0200

    Added @Autowired

--- src/main/java/com/jokes/bjorn/springbootjokesapp/controllers/JokesController.java
+++ src/main/java/com/jokes/bjorn/springbootjokesapp/controllers/JokesController.java
@@ -1,6 +1,7 @@
 package com.jokes.bjorn.springbootjokesapp.controllers;
 
 import com.jokes.bjorn.springbootjokesapp.services.JokesServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ public class JokesController {
 
     private JokesServiceImpl jokesServiceImpl;
 
+    @Autowired
     public JokesController(JokesServiceImpl jokesServiceImpl) {
         this.jokesServiceImpl = jokesServiceImpl;
     }
